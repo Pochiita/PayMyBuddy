cbf3ab36691dd014a3f410d1ef6decd62ebb58ba
pochi <pochi>
2024-04-06 17:41:53 +0200

    Initial commit

---
+++ src/main/java/com/paymybuddy/pochiita/PochiitaApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.pochiita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PochiitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PochiitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymybuddy/pochiita/PochiitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.pochiita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PochiitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
